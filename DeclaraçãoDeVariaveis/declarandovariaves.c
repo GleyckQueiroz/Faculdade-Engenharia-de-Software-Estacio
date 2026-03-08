de5de17712bd206b4d7afa831322f244c28e7275
Gleyck Queiroz <gleyck.queiroz@gmail.com>
2026-03-07 21:22:51 -0300

    Aprendendo a declarar e  inicializar variaveis

---
+++ DeclaraçãoDeVariaveis/declarandovariaves.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+
+    int idade; 
+    float altura;
+    double salario;
+    char opcao;
+    char nome[20];
+
+    /*
+      Os nomes das variáveis devem sempre começar com uma letra 
+      ou um sublinhado.
+      Eles não podem começar com um número.
+
+      As variaveis sempre devem ser inicializadas, isso pode ser feito no 
+      momento da criação da variavel ou depois.
+    */
+
+    idade = 25;
+    altura = 1.70;
+    salario = 1800;
+    opcao = 'y';
+    nome[20] = 'Gleyck';
+
+    return 0;
+}
