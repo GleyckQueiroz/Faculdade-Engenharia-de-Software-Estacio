543ea19230f36951184c5b5bc77ff38fb5f53897
Gleyck Queiroz <gleyck.queiroz@gmail.com>
2026-03-10 19:12:07 -0300

    Adiciona estrutura básica para entrada, cálculo e saída de dados

---
+++ ConceitosDeSolucaoEstruturadas/solucaoestruturada.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+// Função generica para a entrada de dados
+
+void entradaDeDados(){
+  //Código da entrada de dados
+}
+
+//Função gênerica para o calculo da média
+float calculoDaMedia(){
+  //Código do calculo da média
+}
+
+//Função gênerica para a saída de dados
+void exibirMedia(){
+  //Código para a função exibirMedia
+}
+
+int main(){
+
+  entradaDeDados();
+  float media = calculoDaMedia();
+  exibirMedia();
+
+  return 0;
+}
